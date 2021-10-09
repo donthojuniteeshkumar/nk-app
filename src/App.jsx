7c19fb8b585fa9060906ac5631b94c9a4562f4ae
niteesh <niteesh369@outlook.com>
2021-10-09 21:05:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return <div className="App">
+    <button className="border-2 px-2 py-1 border-blue-500 md:border-red-500 rounded bg-blue-500 md:bg-red-500 text-white hover:bg-transparent hover:border-2 hover:text-blue-500 hover:md:text-red-500 outline-none focus:outline-none focus:ring focus:border-blue-300 focus:md:border-red-300">
+      Hello Tailwind
+    </button>
+  </div>;
+}
+
+export default App;
